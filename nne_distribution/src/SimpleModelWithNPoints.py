0407358ee5c25eb8a44c0daae12c926d09943188
saurabh <saurabh_dasgupta@hotmail.com>
2021-04-02 12:21:57 +0100

    Simple model using Python

---
+++ nne_distribution/src/SimpleModelWithNPoints.py
@@ -0,0 +1,23 @@
+from typing import List
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def plot(points_count:List,  means:List):
+    plt.ylim([0, 0.5])
+    plt.scatter(points_count,means, marker='.', s=5)
+    plt.grid(b=True, which='major', color='#666666', linestyle='-')
+
+    plt.show()
+    pass
+
+lst_pointcount=[]
+lst_meandistance=[]
+for point_count in range(2,2000):
+    mean_distance=1/(math.sqrt(point_count)+1)
+    lst_pointcount.append(point_count)
+    lst_meandistance.append(mean_distance)
+    print("Computed mean NN distance of %f for %d points" % (mean_distance,point_count))
+
+plot(lst_pointcount, lst_meandistance)
