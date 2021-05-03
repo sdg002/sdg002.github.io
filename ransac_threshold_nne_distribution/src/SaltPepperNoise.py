b7160c7faf5ee407c3d4294227a8161c316bc77d
saurabh <saurabh_dasgupta@hotmail.com>
2021-05-03 23:16:41 +0100

    Added basic unit testing structure

--- ransac_threshold_nne_distribution/src/SaltPepperNoise.py
+++ ransac_threshold_nne_distribution/src/SaltPepperNoise.py
@@ -11,9 +11,12 @@
 
 import numpy as np
 import random
+from numpy.lib.type_check import imag
 from skimage import io
 import os
 import datetime
+import math;
+import Util
 
 white_color=255
 width=100
@@ -48,17 +51,58 @@ def display_count_of_blackwhite_pixels(image:np.ndarray):
     count_of_white=len(all_white_indices[0])
     count_of_black=image.shape[0]*image.shape[1] - count_of_white
     print("Total white=%d   Total black=%d" % (count_of_white, count_of_black))
+    print("Actual salt_pepper=%f" % (count_of_white/(count_of_white+count_of_black)))
     pass
 
 
+'''
+    max_distance = the maximum allowed distance between 2 consecutive RANSAC points
+'''
+def superimpose_random_straight_line(image:np.ndarray, max_distance:float):
+    image_width=image.shape[1]
+    image_height=image.shape[0]
+    y1=0
+    x1=random.random() * image_width
+
+    y2=image_height-1
+    x2=random.random() * image_width
+
+    #you were here - how to generate the points of a straight line
+    vector=(x2-x1,y2-y1)
+    magnitude=math.sqrt( (vector[0])**2 + (vector[1])**2)
+    uni_vector=(vector[0]/magnitude, vector[1]/magnitude )
+
+    t=0.5
+    x_start=x1
+    y_start=y1
+    while (True):
+        x_new=x1 + t*uni_vector[0]
+        y_new=y1 + t*uni_vector[1]
+        gap=math.sqrt( (x_new-x_start)**2 + (y_new-y_start)**2 )
+        if (gap > max_distance):
+            t=t*0.5
+            continue
+        if (gap <= max_distance):
+            t=t*1.5
+
+        #record x_new and y_new
+        x_start=x_new
+        y_start=y_new
+
+    return image
+
 def generate_image_with_salt_pepper_noise(salt_pepper:float):
     print("--------------------------------------------------")
-    img = np.zeros([width,width,1],dtype=np.uint8)
     print("Generating noisy image with salt_pepper=%f" % (salt_pepper))
-    replace_pixels_with_noise(img,salt_pepper)
+    #
+    noisy_image=Util.generate_noisy_image(width=width, height=width,salt_pepper=salt_pepper)
     new_filename=generate_filename(basename="noisy_image", salt_pepper=salt_pepper)
-    save_image(image=img,filename=new_filename)
-    display_count_of_blackwhite_pixels(img)
+    save_image(image=noisy_image,filename=new_filename)
+    display_count_of_blackwhite_pixels(noisy_image)
+    #
+    # img = np.zeros([width,width,1],dtype=np.uint8)
+    # superimpose_random_straight_line(image=img, max_distance=10)
+    #replace_pixels_with_noise(noisy_image,salt_pepper)
 
 generate_image_with_salt_pepper_noise(salt_pepper=0.5)
 generate_image_with_salt_pepper_noise(salt_pepper=0.6)
---
+++ ransac_threshold_nne_distribution/src/Util.py
@@ -0,0 +1,79 @@
+import numpy as np
+import random
+import math
+
+from numpy.core.numeric import NaN
+from numpy.lib import utils
+
+#
+#Generates a monochrome image with specified width and height.
+#Noise is generated as per the ration salt_pepper. 
+#salt=white pixel and pepper=black pixel
+#
+WHITE_COLOR:int=255
+BLACK_COLOR:int=0
+
+def generate_noisy_image(width:int, height:int,salt_pepper:float):
+    image = np.zeros([height,width,1],dtype=np.uint8)
+    for y in range(0,height):
+        for x in range(0,width):
+            r=random.random()
+            if (r > salt_pepper):
+                image[y][x][0]=0
+            else:
+                image[y][x][0]=WHITE_COLOR
+
+    return image
+
+
+def euclidean_distance(x1:float,y1:float, x2:float,y2:float):
+    return math.sqrt((x1-x2)**2+(y1-y2)**2)
+#
+#Create a dotted line which starts from the specified start point and ends at the specified end point
+#The distance between consecutive points should not exceed max_distance
+#
+def superimpose_straight_line_between_2_points(input_image:np.ndarray,start_x:float, start_y:float, end_x:float, end_y:float, max_distance:float):
+    width=input_image.shape[1]
+    height=input_image.shape[0]
+
+    distance_start_end=euclidean_distance(x1=start_x, y1=start_y, x2=end_x, y2=end_y)
+    unit_vector=((end_x-start_x)/distance_start_end, (end_y-start_y)/distance_start_end)
+
+    delta=distance_start_end/2 #some random value
+    last_t=0
+    max_t=distance_start_end
+    last_point=(start_x,start_y)
+    min_distance=max_distance/2
+
+    results=list()
+
+    while True:
+        new_t=last_t+delta
+        if (new_t > max_t):
+            break
+        next_point=(start_x+unit_vector[0]*new_t,start_y+unit_vector[1]*new_t)
+        consecutive_distance=euclidean_distance(x1=last_point[0], y1=last_point[1], x2=next_point[0], y2=next_point[1])
+        if (consecutive_distance < max_distance and consecutive_distance > min_distance):
+            results.append(next_point)
+            last_t=new_t
+            last_point=next_point
+            continue
+        elif (consecutive_distance <= min_distance):
+            delta=delta*1.5
+            continue
+        elif (consecutive_distance >= max_distance):
+            delta=delta*0.5
+            continue
+        else:
+            continue
+
+    for point in results:
+        x=int(round(point[0]))
+        y=int(round(height-point[1]))
+        if (x<0 or x >= width):
+            continue
+        if (y<0 or y >= height):
+            continue
+        input_image[y][x][0]=BLACK_COLOR
+    return input_image
+    pass
