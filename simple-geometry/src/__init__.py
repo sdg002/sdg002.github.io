d8b0028ed9e695120de740d8fed9b06b1c79af32
saurabh <saurabh_dasgupta@hotmail.com>
2021-08-14 17:37:49 +0100

    Added Vectory

--- simple-geometry/src/__init__.py
+++ simple-geometry/src/__init__.py
@@ -1,5 +1,6 @@
 from .Point import Point
 from .LineModel import LineModel
 from .CircleModel import CircleModel
+from .Vector import Vector
 
-__all__=["Point","LineModel","CircleModel"]
+__all__=["Point","LineModel","CircleModel","Vector"]
