bc0687cd40db62337aaccd938b4080bfd4a99c80
saurabh <saurabh_dasgupta@hotmail.com>
2021-10-10 20:57:28 +0100

    Completed adding multiple customers in a single txn

--- sqlitedemo/createdatabase.py
+++ sqlitedemo/createdatabase.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+import inspect
 
 Base = declarative_base()
 
@@ -21,16 +22,27 @@ class Customers(Base):
 
 def add_customer(session):
     now=datetime.now()
-    c1=Customers(name=f"john {now.second}", address="blah address", email=f"john-{now.microsecond}@cool.com")
+    c1=Customers(name=f"john {now.microsecond}", address="blah address", email=f"john-{now.microsecond}@cool.com")
     session.add(c1)
     session.commit()
     print(f"Added new customer with the name {c1.name}")
+    return c1
+    pass
+
+def add_multiple_customers(session):
+    count=7
+    now=datetime.now()
+    for index in range(0,count):
+        c1=Customers(name=f"john {now.second+index}", address="blah address", email=f"john-{now.second+index}@cool.com")
+        print(f"Added new customer with the name {c1.name}")
+        session.add(c1)
+    session.commit()
     pass
 
 def query_all_customers(session):
+ 
     result = session.query(Customers).all()
     for row in result:
-        #print (f"Name: {row.name} Address: {row.address} Email: {row.email}")
         print(row)
 
 def delete_all_customers(connection):
@@ -48,10 +60,10 @@ def main():
     Session = sessionmaker(bind = engine)
     session = Session()
     delete_all_customers(connection=connection)
-    add_customer(session)
-    add_customer(session)
-    add_customer(session)
-    add_customer(session)
+    add_multiple_customers(session)
+    #c=add_customer(session)
+    #print(inspect.getmembers(c)) #how to get all members
+    #print(inspect.getmembers, inspect.isfunction) #when you want all function members only
     query_all_customers(session)
 
 if (__name__ =="__main__"):
